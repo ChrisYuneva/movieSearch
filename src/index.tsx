88e63dfb0bb6119f59a284e31ba68ee0779301d1
ChrisYuneva <christina_yuneva@mail.ru>
2024-04-08 18:55:14 +0400

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from './store/store';
+import { Provider } from 'react-redux';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+     <Provider store={store}>
+      <RouterProvider router={router} />
+     </Provider>
+  </React.StrictMode>
+);
